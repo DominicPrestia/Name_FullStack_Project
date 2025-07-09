9eb97708af1f4be573b60b073e05daed470ac195
Dominic <dominicprestia0@gmail.com>
2025-07-08 19:08:45 -0500

    starting login page

--- clientSideApp/src/App.jsx
+++ clientSideApp/src/App.jsx
@@ -1,13 +1,19 @@
+import { Route, Routes } from 'react-router-dom'
 import Form from '../src/components/form/Form'
 import DataDisplay from './components/dataDisplay/DataDisplay'
+import Login from './pages/Login'
+import NameEntry from './pages/NameEntry'
+
 
 function App() {
 
   return (
-    <div style={{display:'flex',flexDirection:'column', gap:'20px', alignItems:'center', minHeight:'100vh'}}>
-      <h1 style={{margin:'50px'}}>First Full Stack Application</h1>
-      <Form/>
-      <DataDisplay/>
+    <div style={{ display: 'flex', flexDirection: 'column', gap: '20px', alignItems: 'center', minHeight: '100vh' }}>
+      <h1 style={{ margin: '50px' }}>First Full Stack Application</h1>
+      <Routes>
+        <Route path='/' element={<Login />}/>
+        <Route path ='/nameentry' element={<NameEntry />}/>
+      </Routes>
     </div>
   )
 }
---
+++ clientSideApp/src/pages/NameEntry.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Form from '../components/form/Form'
+import DataDisplay from '../components/dataDisplay/DataDisplay'
+
+const nameEntry = () => {
+  return (
+    <div>
+      <Form/>
+      <DataDisplay/>
+    </div>
+  )
+}
+
+export default nameEntry
